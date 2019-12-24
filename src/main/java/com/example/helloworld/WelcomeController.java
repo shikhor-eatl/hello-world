a7eeefe588415008a82768e36dcbd6d61666cfc7
Shikho Roy <shikhor@eatlbd.com>
2019-12-24 17:22:15 +0600

    Init.

---
+++ src/main/java/com/example/helloworld/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class WelcomeController {
+    @RequestMapping("/hello")
+    Object sayHello() {
+        return "Hello World";
+    }
+}
